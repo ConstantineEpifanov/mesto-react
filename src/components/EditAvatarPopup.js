8689b66bf01fc8bfe48a969f054491bda22efdaa
Constantine Epifanov <asbest911@gmail.com>
2023-04-05 08:59:20 +0300

    fix reset form

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
@@ -10,6 +10,10 @@ export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
       avatar: avatar.current.value,
     });
   }
+
+  useEffect(() => {
+    avatar.current.value = "";
+  }, [isOpen]);
 
   return (
     <PopupWithForm
